5c94e6efd65638455f156173cbb8551430f3da1b
samielsabri <sami.elsabri@mail.utoronto.ca>
2024-01-19 00:15:09 -0500

    wrote data cleaning script which writes into output folder

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,46 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw plane data recorded by opendatatoronto
+# Author: Sami El Sabri
+# Date: 19 January 2024
+# Contact: sami.elsabri@mail.utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-Requisites: 01-download_data.R
 
 #### Workspace setup ####
 library(tidyverse)
+library(stringr)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("inputs/data/raw_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+cleaned_data <- raw_data %>% select(Asset.ID, 
+                                    Asset.Name, 
+                                    Pad.Length..ft.., 
+                                    Pad.Width..ft..,
+                                    Boards..Ice.Rink.) 
+
+
+cleaned_data <- cleaned_data %>% 
+  rename(
+    id = Asset.ID,
+    name = Asset.Name,
+    length = Pad.Length..ft..,
+    width = Pad.Width..ft..,
+    has_boards = Boards..Ice.Rink.
+  )
+
+cleaned_data <- cleaned_data %>% 
+  filter(!str_detect(name, "Trail"))
+
+# impute zero length/width value
+mean_width <- round(mean(cleaned_data$width[cleaned_data$width != 0 &
+                                        !is.na(cleaned_data$width)]))
+
+cleaned_data <- cleaned_data %>% 
+  mutate(width =ifelse(width == 0, mean_width, width))
+
+cleaned_data <- cleaned_data %>% 
+  mutate(size = ifelse(is.na(length), NA, length * width) )
 
 #### Save data ####
 write_csv(cleaned_data, "outputs/data/analysis_data.csv")
